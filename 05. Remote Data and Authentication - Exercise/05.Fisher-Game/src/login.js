e18d4cc3ff4124cdcf47e9e2848e0f30104329fa
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-10-03 15:27:56 +0300

    Remote Data and Authentication

---
+++ 05. Remote Data and Authentication - Exercise/05.Fisher-Game/src/login.js
@@ -0,0 +1,46 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', login);
+
+async function login(e) {
+    e.preventDefault();
+
+    let formData = new FormData(e.target);
+
+    let email = formData.get('email');
+    let password = formData.get('password');
+
+    if (email == '' || password == '') {
+        form.querySelector('.notification').textContent = 'All fields are required!'
+        return;
+    }
+
+    try {
+        let response = await fetch('http://localhost:3030/users/login', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                email,
+                password
+            })
+        })
+        let data = await response.json();
+
+        if (!response.ok || response.status != 200) {
+            form.reset();
+            throw new Error(data.message);
+        }
+
+        console.log(data);
+        sessionStorage.setItem('accessToken', data.accessToken);
+        sessionStorage.setItem('email', data.email);
+        sessionStorage.setItem('username', data.username);
+        sessionStorage.setItem('_id', data._id);
+        window.location.href = './index.html'
+            
+    } catch (error) {
+        form.querySelector('.notification').textContent = error.message;
+    }
+}
