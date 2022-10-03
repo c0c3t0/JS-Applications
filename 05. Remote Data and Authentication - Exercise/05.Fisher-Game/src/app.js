c5a6602b0e17267a42f0064b6f849ddd085b6ec3
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-10-03 23:35:18 +0300

    Remote Data and Authentication

--- 05. Remote Data and Authentication - Exercise/05.Fisher-Game/src/app.js
+++ 05. Remote Data and Authentication - Exercise/05.Fisher-Game/src/app.js
@@ -2,7 +2,48 @@ const userNav = document.querySelector('#user');
 const guestNav = document.querySelector('#guest');
 const greetingMsg = document.querySelector('.email span');
 const logoutBtn = document.querySelector('#logout');
+const loadBtn = document.querySelector('.load');
+const catches = document.querySelector('#catches');
 
+
+async function getCatches() {
+    try {
+        let response = await fetch('http://localhost:3030/data/catches');
+        let data = await response.json();
+        
+        if(!response.ok || response.status != 200) {
+            throw new Error(data.message);
+        }
+
+        catches.replaceChildren();
+        Object.values(data).forEach(x => {
+            let catchDiv = htmlGenerator('div', '', catches, 'catch');
+            htmlGenerator('label', 'Angler', catchDiv);
+            htmlGenerator('input', '', catchDiv, 'angler', 'text', `${x.angler}`);
+            htmlGenerator('label', 'Weight', catchDiv);
+            htmlGenerator('input', '', catchDiv, 'weight', 'text', `${x.weight}`);
+
+            htmlGenerator('label', 'Species', catchDiv);
+            htmlGenerator('input', '', catchDiv, 'species', 'text', `${x.species}`);
+
+            htmlGenerator('label', 'Location', catchDiv);
+            htmlGenerator('input', '', catchDiv, 'location', 'text', `${x.location}`);
+            htmlGenerator('label', 'Bait', catchDiv);
+            htmlGenerator('input', '', catchDiv, 'bait', 'text', `${x.bait}`);
+            htmlGenerator('label', 'Capture Time', catchDiv);
+            htmlGenerator('input', '', catchDiv, 'captureTime', 'text', `${x.captureTime}`);
+            let updateBtn = htmlGenerator('button', 'Update', catchDiv, 'update');
+            updateBtn.setAttribute('data-id', `${x._id}`);
+            let deleteBtn = htmlGenerator('button', 'Delete', catchDiv, 'delete');
+            deleteBtn.setAttribute('data-id', `${x._id}`);
+
+
+
+        })
+    } catch (error) {
+        
+    }
+}
 async function logout() {
     await fetch('http://localhost:3030/users/logout', {
         method: 'GET',
@@ -26,5 +67,24 @@ window.addEventListener('load', async () => {
         guestNav.style.display = 'inline-block';
         greetingMsg.textContent = 'guest';
     }
+    loadBtn.addEventListener('click', getCatches);
 })
 
+function htmlGenerator(tag, text, parent, className, type, value) {
+    let el = document.createElement(tag);
+    el.textContent = text;
+
+    if (parent) {
+        parent.appendChild(el);
+    }
+    if (className) {
+        el.className = className;
+    }
+    if (type) {
+        el.type = type;
+    }
+    if (value) {
+        el.value = value;
+    }
+    return el;
+}
