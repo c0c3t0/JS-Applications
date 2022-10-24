b9484811d682ab1b1108202edaf8df0cd35fd347
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-10-24 17:28:27 +0300

    Remote Data and Authentication

--- 05. Remote Data and Authentication - Exercise/02.Phonebook/app.js
+++ 05. Remote Data and Authentication - Exercise/02.Phonebook/app.js
@@ -1,49 +1,51 @@
-const url = 'http://localhost:3030/jsonstore/phonebook';
-
-const phonebook = document.querySelector('#phonebook');
-const person = document.querySelector('#person');
-const phone = document.querySelector('#phone');
-
 function attachEvents() {
     document.querySelector('#btnLoad').addEventListener('click', getPhones);
     document.querySelector('#btnCreate').addEventListener('click', setPhone);
 }
 
 async function getPhones() {
+    const url = 'http://localhost:3030/jsonstore/phonebook';
+    const phonebook = document.querySelector('#phonebook');
+    phonebook.replaceChildren();
+
     const response = await fetch(url);
     const data = await response.json();
 
-    phonebook.replaceChildren();
     Object.values(data).forEach(x => {
         let li = htmlGenerator('li', `${x.person}: ${x.phone}`, phonebook);
         let deleteBtn = htmlGenerator('button', 'Delete', li);
-        deleteBtn.id = x._id;
+        deleteBtn.setAttribute('id', x._id);
         deleteBtn.addEventListener('click', (e) => deletePhone(e));
     })
 }
 
 async function setPhone() {
+    const url = 'http://localhost:3030/jsonstore/phonebook';
+    const person = document.querySelector('#person');
+    const phone = document.querySelector('#phone');
+
     if (person.value && phone.value) {
         let info = {
-            person: person.value,
-            phone: phone.value
+            "person": person.value,
+            "phone": phone.value
         }
         await request(url, info);
-        getPhones();
 
+        getPhones();
         person.value = '';
         phone.value = '';
-
-    } else{
+    } else {
         alert('All fields are required');
     }
 }
 
 async function deletePhone(e) {
-    let id = e.target.id;
+    const url = 'http://localhost:3030/jsonstore/phonebook';
+
+    let id = e.target.getAttribute('id');
     e.target.parentNode.remove();
 
-    await fetch(`${url}/${id}}`, {
+    await fetch(`${url}/${id}`, {
         method: 'DELETE',
     });
 }
