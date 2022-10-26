059e7da431fba6da4edfade45494a6be804535e0
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-10-27 02:18:19 +0300

    final changes

--- 03. Asynchronous Programming - Exercise/06.Blog/app.js
+++ 03. Asynchronous Programming - Exercise/06.Blog/app.js
@@ -1,84 +1,76 @@
-let baseUrl = 'http://localhost:3030/jsonstore/blog';
-let posts = document.querySelector("#posts");
-let postTitle = document.querySelector("#post-title");
-let postBody = document.querySelector("#post-body");
-let comments = document.querySelector("#post-comments");
-
 function attachEvents() {
+    const baseUrl = 'http://localhost:3030/jsonstore/blog/';
+    const posts = document.querySelector("#posts");
+    const postTitle = document.querySelector("#post-title");
+    const postBody = document.querySelector("#post-body");
+    const comments = document.querySelector("#post-comments");
+
     document.querySelector("#btnLoadPosts").addEventListener('click', () => getPosts());
-    document.querySelector("#btnViewPost").addEventListener('click', () => viewPostInfo());
-    document.querySelector("#btnViewPost").addEventListener('click', () => viewComments());
-}
+    document.querySelector("#btnViewPost").addEventListener('click', () => viewPostDetails());
 
-async function getPosts() {
-    try {
-        let response = await fetch(`${baseUrl}/posts`);
-        if (!response.ok) {
-            throw new Error('Error');
-        }
-        let data = await response.json();
-
-        Object.values(data).forEach(x => {
-            htmlGenerator('option', x.title, posts, x.id);
-        });
-
-    } catch (error) {
-        console.error(error.message);
-    }
-}
-
-async function viewPostInfo() {
-    try {
-        let id = posts.value;
-        let res = await fetch(`${baseUrl}/posts/${id}`);
-        if (!res.ok) {
-            throw new Error('Error');
-        }
-        let data = await res.json();
-
-        postTitle.textContent = data.title;
-        postBody.textContent = data.body;
-
-    } catch (error) {
-        console.error(error.message);
-    }
-}
-
-async function viewComments() {
-    try {
-        let id = posts.value;
-
-        let response = await fetch(`${baseUrl}/comments`);
-        if (!response.ok) {
-            throw new Error('Error');
-        }
-        let info = await response.json();
-
-        comments.replaceChildren();
-        Object.values(info).forEach(x => {
-            if (id == x.postId) {
-                let li = htmlGenerator('li', x.text, comments);
-                li.id = x.id;
+    async function getPosts() {
+        try {
+            const response = await fetch(`${baseUrl}posts`);
+            if (!response.ok) {
+                throw new Error('Error');
             }
-        });
+            const data = await response.json();
 
-    } catch (error) {
-        console.error(error.message);
-    }
-}
-
-function htmlGenerator(tagName, content, parent, value) {
-    let el = document.createElement(tagName);
-    el.textContent = content;
-
-    if (parent) {
-        parent.appendChild(el);
+            posts.replaceChildren();
+            Object.values(data).forEach(x => {
+                htmlGenerator('option', x.title, posts, x.id);
+            });
+        } catch (error) {
+            console.error(error.message);
+        }
     }
 
-    if (value) {
-        el.value = value;
+    async function viewPostDetails() {
+        try {
+            const res = await fetch(`${baseUrl}posts`);
+            if (!res.ok) {
+                throw new Error('Error');
+            }
+            const allPosts = await res.json();
+
+            const id = posts.value;
+            const title = posts.options[posts.selectedIndex].text;
+            const body = Object.values(allPosts).find(x => x.title === title);
+
+            postTitle.textContent = title;
+            postBody.textContent = body.body;
+
+            const response = await fetch(`${baseUrl}comments`);
+            if (!response.ok) {
+                throw new Error('Error');
+            }
+            const info = await response.json();
+
+            comments.replaceChildren();
+            Object.values(info).forEach(x => {
+                if (id === x.postId) {
+                    const li = htmlGenerator('li', x.text, comments);
+                    li.id = x.id;
+                }
+            });
+        } catch (error) {
+            console.error(error.message);
+        }
     }
-    return el;
+
+    function htmlGenerator(tagName, content, parent, value) {
+        const el = document.createElement(tagName);
+        el.textContent = content;
+
+        if (parent) {
+            parent.appendChild(el);
+        }
+
+        if (value) {
+            el.setAttribute('value', value);
+        }
+        return el;
+    }
 }
 
 attachEvents();
