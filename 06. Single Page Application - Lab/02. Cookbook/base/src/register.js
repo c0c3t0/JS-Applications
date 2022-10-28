f691613bbff9b57e78c8455aebde5a83c286b3cf
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-10-29 02:02:48 +0300

    Changes

--- 06. Single Page Application - Lab/02. Cookbook/base/src/register.js
+++ 06. Single Page Application - Lab/02. Cookbook/base/src/register.js
@@ -1,19 +1,35 @@
-const form = document.querySelector('form');
+import { showCatalog } from "./catalog.js";
+import { showSection } from "./dom.js";
 
-form.addEventListener('submit', (e) => {
+const section = document.querySelector('#register');
+
+const registerForm = document.querySelector('#register form');
+registerForm.addEventListener('submit', registerUser);
+
+export function showRegister() {
+    showSection(section);
+}
+
+async function registerUser(e) {
     e.preventDefault();
 
     let formData = new FormData(e.target);
+
     let email = formData.get('email');
     let password = formData.get('password');
     let rePass = formData.get('rePass');
 
-    try {
-        if (password !== rePass) {
-            throw new Error('Error');
-        }
+    if (email === '' || password === '' || rePass === '') {
+        alert('All fields are required!');
+        return;
+    }
+    if(password !== rePass){
+        alert('Passwords don\'t match');
+        return;
+    }
 
-        fetch('http://localhost:3030/users/register', {
+    try {
+        const response = await fetch('http://localhost:3030/users/register', {
             method: 'POST',
             headers: {
                 'content-type': 'application/json'
@@ -22,18 +38,24 @@ form.addEventListener('submit', (e) => {
                 email,
                 password
             })
-        })
-            .then(response => {
-                if (response.status != 200) {
-                    throw new Error('Error');
-                }
-                return response.json();
-            })
-            .then(user => {
-                sessionStorage.setItem('accessToken', user.accessToken);
-                window.location.pathname = '/02. Cookbook/base/index.html';
-            })
+        });
+        const data = await response.json();
+        if (!response.ok) {
+            form.reset();
+            throw new Error(data.message);
+        }
+
+        sessionStorage.setItem('accessToken', data.accessToken);
+        sessionStorage.setItem('userId', data._id);
+        sessionStorage.setItem('userEmail', data.email);
+
+        document.querySelector('#user').style.display = 'inline-block';
+        document.querySelector('#guest').style.display = 'none';
+
+        showCatalog();
+
     } catch (error) {
-        console.error(error.message);
+        alert(error.message);
     }
-})
+
+}
