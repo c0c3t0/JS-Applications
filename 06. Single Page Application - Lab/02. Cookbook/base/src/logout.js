6352a8a8b9ef010fd595cb27af97550eceb44496
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-10-29 01:20:41 +0300

    Changes

---
+++ 06. Single Page Application - Lab/02. Cookbook/base/src/logout.js
@@ -0,0 +1,27 @@
+import { showCatalog } from "./catalog.js";
+
+export async function logout() {
+    try {
+        const response = await fetch('http://localhost:3030/users/logout', {
+            method: 'GET',
+            headers: {
+                'X-Authorization': sessionStorage.getItem('accessToken')
+            }
+        });
+
+        if (response.ok) {
+            sessionStorage.removeItem('accessToken');
+            document.querySelector('#user').style.display = 'none';
+            document.querySelector('#guest').style.display = 'inline-block';
+
+            showCatalog();
+        } else {
+            throw new Error(await response.json());
+        }
+
+    } catch (error) {
+        alert(error.message)
+    }
+
+}
+
