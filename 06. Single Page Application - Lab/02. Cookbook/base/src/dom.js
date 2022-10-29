9b7ee3d8cf4f9deec7e48ec097233d876aed05c3
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-10-30 02:50:51 +0300

    Changes

--- 06. Single Page Application - Lab/02. Cookbook/base/src/dom.js
+++ 06. Single Page Application - Lab/02. Cookbook/base/src/dom.js
@@ -1,7 +1,12 @@
 const main = document.querySelector('main');
+const nav = document.querySelector('nav');
 
 export function showSection(section) {
     main.replaceChildren(section);
+}
+
+export function setActiveNav(targetId) {
+    [...nav.querySelectorAll('a')].forEach(a => a.id == targetId ? a.classList.add('active') : a.classList.remove('active'));
 }
 
 export function htmlGenerator(type, text, className, parent) {
