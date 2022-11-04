ae09d883e3d079613a112e09bdf73ec1df6c5ba4
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-04 03:46:10 +0200

    Initial commit

--- 07. Single Page Application - Exercise/01.Forum/src/load.js
+++ 07. Single Page Application - Exercise/01.Forum/src/load.js
@@ -1,56 +1,44 @@
-import { showComments } from './comments.js';
-import { createTopic } from './create.js';
-import { htmlGenerator } from './dom.js';
+import { createTopicHTML } from './dom.js';
+import { createTopicDetailsHTML } from './dom.js';
+import { getComments } from './comments.js';
+
+
+const topics = document.querySelector('.topic-container');
 
 export async function getTopics() {
-    const response = await fetch('http://localhost:3030/jsonstore/collections/myboard/posts');
-    const data = await response.json();
+    try {
+        const response = await fetch('http://localhost:3030/jsonstore/collections/myboard/posts');
+        const data = await response.json();
 
-    Object.values(data).forEach(post => {
-        console.log(post);
+        if (!response.ok) {
+            throw new Error(response.message);
+        }
 
-        const divTopicNameWrapper = htmlGenerator('div', '', 'topic-name-wrapper', document.querySelector('.topic-container'));
-        divTopicNameWrapper.id = post._id;
+        topics.replaceChildren();
+        Object.values(data)
+            .forEach(post => {
+                const topic = createTopicHTML(post);
+                topic.addEventListener('click', (e) => showSelectedTopic(e.currentTarget.id));
+            });
 
-        const divTopicName = htmlGenerator('div', '', 'topic-name', divTopicNameWrapper);
-        const aNormal = htmlGenerator('a', '', 'normal', divTopicName);
-        aNormal.setAttribute('href', '#');
-        htmlGenerator('h2', post.title, '', aNormal);
-
-        const divColumns = htmlGenerator('div', '', 'columns', divTopicName);
-        const div1 = htmlGenerator('div', '', '', divColumns);
-        const p = htmlGenerator('p', 'Date: ', '', div1);
-        htmlGenerator('time', post.date, '', p);
-
-        const divNickname = htmlGenerator('div', '', 'nick-name', div1);
-        const pUsername = htmlGenerator('p', 'Username: ', '', divNickname);
-        htmlGenerator('span', post.username, '', pUsername);
-
-        divTopicNameWrapper.addEventListener('click', () => showComments(post));
-    });
+    } catch (error) {
+        alert(error.message);
+    }
 }
 
-const form = document.querySelector('form');
-form.addEventListener('submit', getFormData);
+export async function showSelectedTopic(id) {
+    // try {
+    const response = await fetch(`http://localhost:3030/jsonstore/collections/myboard/posts/${id}`);
+    const data = await response.json();
 
-function getFormData(e) {
-    e.preventDefault();
-
-    const dataForm = new FormData(e.target);
-    const title = dataForm.get('topicName');
-    const username = dataForm.get('username');
-    const post = dataForm.get('postText');
-
-    if (e.submitter.textContent === 'Post') {
-        if (!title || !username || !post) {
-            alert('All fields are required!');
-            return;
-        }
-        createTopic(title, username, post);
-        // getTopics();
-        form.reset();
-
-    } else if (e.submitter.textContent === 'Cancel') {
-        form.reset();
+    if (!response.ok) {
+        throw new Error(response.message);
     }
+    const topic = createTopicDetailsHTML(data);
+    getComments(topic.id);
+
+
+    // } catch (error) {
+    //     alert(error.message);
+    // }
 }
