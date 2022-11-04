11b5a56fd807b36004054061b8bbecb610f1eec8
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-04 03:45:42 +0200

    Initial commit

--- 07. Single Page Application - Exercise/01.Forum/src/create.js
+++ 07. Single Page Application - Exercise/01.Forum/src/create.js
@@ -1,17 +1,46 @@
-export async function createTopic(title, username, post) {
-    const response = await fetch('http://localhost:3030/jsonstore/collections/myboard/posts', {
-        method: 'POST',
-        headers: { 'Content-type': 'application/json' },
-        body: JSON.stringify({
-            title,
-            username,
-            post,
-            date: new Date
-        })
-    });
+import { getTopics } from './load.js';
 
-    if (!response.ok) {
-        throw new Error(response.message);
+const form = document.querySelector('form');
+
+export function getFormData(e) {
+    e.preventDefault();
+
+    const dataForm = new FormData(e.target);
+    const title = dataForm.get('topicName');
+    const username = dataForm.get('username');
+    const post = dataForm.get('postText');
+
+    if (e.submitter.textContent === 'Post') {
+        if (!title || !username || !post) {
+            alert('All fields are required!');
+            return;
+        }
+        createTopic(title, username, post);
     }
-    const data = await response.json();
+    form.reset();
+}
+
+
+async function createTopic(title, username, post) {
+    try {
+        const response = await fetch('http://localhost:3030/jsonstore/collections/myboard/posts', {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify({
+                title,
+                username,
+                post,
+                date: new Date
+            })
+        });
+
+        if (!response.ok) {
+            const data = await response.json();
+            throw new Error(data.message);
+        }
+        getTopics();
+
+    } catch (error) {
+        alert(error.message);
+    }
 }
