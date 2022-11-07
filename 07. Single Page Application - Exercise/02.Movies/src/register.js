c39fc1e7235fff421a26abde3482544d8ce193c3
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-07 20:17:20 +0200

    Added Auth Functionality

--- 07. Single Page Application - Exercise/02.Movies/src/register.js
+++ 07. Single Page Application - Exercise/02.Movies/src/register.js
@@ -1,7 +1,56 @@
+import { showHome } from "./app.js";
 import { showSection } from "./dom.js";
 
 const sections = document.querySelectorAll('.view-section');
+const registerForm = document.querySelector('#register-form');
 
 export function showRegister() {
     showSection(sections[5]);
+    registerForm.addEventListener('submit', registerUser);
+}
+
+async function registerUser(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const email = formData.get('email');
+    const password = formData.get('password');
+    const repeatPassword = formData.get('repeatPassword');
+
+    if (email === '' || password === '' || repeatPassword === '') {
+        alert('All fields are required!');
+        return;
+    }
+    if (password !== repeatPassword) {
+        alert('Passwords don\'t match');
+        return;
+    }
+
+    try {
+        const response = await fetch('http://localhost:3030/users/register', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                email,
+                password
+            })
+        });
+        const data = await response.json();
+        
+        if (!response.ok) {
+            form.reset();
+            throw new Error(data.message);
+        }
+
+        sessionStorage.setItem('accessToken', data.accessToken);
+        sessionStorage.setItem('userId', data._id);
+        sessionStorage.setItem('userEmail', data.email);
+
+        showHome();
+
+    } catch (error) {
+        alert(error.message);
+    }
 }
