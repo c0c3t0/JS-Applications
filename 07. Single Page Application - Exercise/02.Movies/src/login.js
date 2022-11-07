3e61a5dac9978699e0a48e33c57360081ff2741a
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-07 20:17:39 +0200

    Added Auth Functionality

--- 07. Single Page Application - Exercise/02.Movies/src/login.js
+++ 07. Single Page Application - Exercise/02.Movies/src/login.js
@@ -1,7 +1,50 @@
 import { showSection } from "./dom.js";
+import { showHome } from "./app.js";
 
 const sections = document.querySelectorAll('.view-section');
+const loginForm = document.querySelector('#login-form');
 
 export function showLogin() {
     showSection(sections[4]);
+    loginForm.addEventListener('submit', loginUser);
+}
+
+async function loginUser(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    if (email === '' || password === '') {
+        alert('All fields are required!');
+        return;
+    }
+
+    try {
+        const response = await fetch('http://localhost:3030/users/login', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                email,
+                password
+            })
+        });
+        const data = await response.json();
+        if (!response.ok) {
+            form.reset();
+            throw new Error(response.message);
+        }
+
+        sessionStorage.setItem('accessToken', data.accessToken);
+        sessionStorage.setItem('userId', data._id);
+        sessionStorage.setItem('userEmail', data.email);
+
+        showHome();
+
+    } catch (error) {
+        alert(error.message);
+    }
 }
