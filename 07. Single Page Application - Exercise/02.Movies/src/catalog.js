826a235bad608971dd71c936d746e92f8c0d31d6
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-08 15:43:46 +0200

    Added CRUD

--- 07. Single Page Application - Exercise/02.Movies/src/catalog.js
+++ 07. Single Page Application - Exercise/02.Movies/src/catalog.js
@@ -1,17 +1,22 @@
 import { htmlGenerator } from "./dom.js";
+import { showDetails } from "./details.js";
 
 const homePageSection = document.querySelectorAll('.view-section')[0];
-const div = homePageSection.querySelector('#movie div div');
+const movieSection = homePageSection.querySelector('#movie');
 
 export async function getAllMovies() {
-
-    // try {
+    try {
         const response = await fetch('http://localhost:3030/data/movies')
         const data = await response.json();
 
         if (!response.ok) {
             throw new Error(response.message);
         }
+
+        movieSection.replaceChildren();
+        const divMt3 = htmlGenerator('div', '', 'mt-3', movieSection);
+        const divRow = htmlGenerator('div', '', 'row d-flex d-wrap', divMt3);
+        const divCardDeck = htmlGenerator('div', '', 'card-deck d-flex justify-content-center', divRow);
 
         Object.values(data)
             .forEach(info => {
@@ -23,38 +28,39 @@ export async function getAllMovies() {
                 //         </div>
                 //         <div class="card-footer">
                 //             <a href="#/details/6lOxMFSMkML09wux6sAF">
-                            // <button type="button" class="btn btn-info">Details</button></a>
+                //             <button type="button" class="btn btn-info">Details</button></a>
                 //         </div>
-        
                 //     </div>
 
-                const divCard = htmlGenerator('div', '', 'card mb-4', div);
+                const divCard = htmlGenerator('div', '', 'card mb-4', divCardDeck);
 
-                const imgElement = htmlGenerator('img', '', 'card-img-top', divCard);
-                imgElement.setAttribute('src', info.img);
-                imgElement.alt = 'Card image cap';
-                imgElement.width = '400';
+                const img = htmlGenerator('img', '', 'card-img-top', divCard);
+                img.setAttribute('src', info.img);
+                img.alt = 'Card image cap';
+                img.width = '400';
 
                 const divCardBody = htmlGenerator('div', '', 'card-body', divCard);
                 htmlGenerator('h4', info.title, 'card-title', divCardBody);
 
                 const divCardFooter = htmlGenerator('div', '', 'card-footer', divCard);
-                const anchorElement = htmlGenerator('a', '', '', divCardFooter);
-                anchorElement.setAttribute('href', '#');
+                const anchor = htmlGenerator('a', '', '', divCardFooter);
+                anchor.setAttribute('href', '#');
 
                 const detailsBtn = htmlGenerator('button', 'Details', 'btn btn-info', divCardFooter);
                 detailsBtn.type = 'button';
-                detailsBtn.setAttribute('data-id', info._id);
+                detailsBtn.setAttribute('id', info._id);
 
-                // detailsBtn.addEventListener('click', showDetails); // Click on a movie
+                if (!sessionStorage.getItem('accessToken')) {
+                    detailsBtn.setAttribute('disabled', 'disabled');
+                }
+                detailsBtn.addEventListener('click', () => showDetails(info._id));
             });
 
         if (!response.ok) {
             throw new Error(response.message);
         }
 
-    // } catch (error) {
-    //     alert(error.message);
-    // }
-    
+    } catch (error) {
+        alert(error.message);
+    }
 }
