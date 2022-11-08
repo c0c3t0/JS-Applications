f0d0f7ef29401abb5b90cb00690a5b1a5ab253e6
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-08 15:44:00 +0200

    Added CRUD

---
+++ 07. Single Page Application - Exercise/02.Movies/src/delete.js
@@ -0,0 +1,23 @@
+import { showHome } from "./app.js";
+
+export async function deleteMovie(id) {
+    const token = sessionStorage.getItem('accessToken');
+
+    try {
+        const response = await fetch(`http://localhost:3030/data/movies/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'X-Authorization': token
+            }
+        });
+
+        if (response.status != 200) {
+            const error = await response.json();
+            throw new Error(error.message);
+        }
+        showHome();
+
+    } catch (error) {
+        alert(error.message);
+    }
+}
