7eeabf842febdae68a7a1ff557553b0e38fe3bc2
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-09 01:16:50 +0200

    Added Changes

--- 07. Single Page Application - Exercise/02.Movies/src/delete.js
+++ 07. Single Page Application - Exercise/02.Movies/src/delete.js
@@ -2,8 +2,7 @@ import { showHome } from "./app.js";
 
 export async function deleteMovie(e) {
     const token = sessionStorage.getItem('accessToken');
-    console.log(e.target.id);
-    let id = e.target.id
+    const id = e.target.id;
     try {
         const response = await fetch(`http://localhost:3030/data/movies/${id}`, {
             method: 'DELETE',
