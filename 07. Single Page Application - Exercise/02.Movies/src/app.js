12950f943382fbaea02def7aec8da5fec164ce6b
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-08 15:43:37 +0200

    Added CRUD

--- 07. Single Page Application - Exercise/02.Movies/src/app.js
+++ 07. Single Page Application - Exercise/02.Movies/src/app.js
@@ -9,42 +9,26 @@ window.addEventListener('load', showHome);
 
 const nav = document.querySelector('nav');
 const sections = document.querySelectorAll('.view-section');
-// sections = [
-//    V homeSection,
-//    V addMovieSection,
-//      movieExampleSection,
-//      editMovieSection,
-//    V formLoginSection,
-//    V formSignInSection
 
 export function showHome() {
     showNavigation();
-
-    if (!sessionStorage.getItem('accessToken')) {
-        sections[0].querySelector('h1.text-center').style.display = 'none';
-        sections[0].querySelector('section#add-movie-button').style.display = 'none';
-    } else {
-        sections[0].querySelector('h1.text-center').style.display = 'block';
-        sections[0].querySelector('section#add-movie-button').style.display = 'block';
-    }
-
     showSection(sections[0]);
     document.querySelector('#add-movie-button a').addEventListener('click', showCreateMovie);
     getAllMovies();
 }
 
-function showNavigation() {
-    navLinks();
-    // TODO: add greeting message
+export function showNavigation() {
     if (!sessionStorage.getItem('accessToken')) {
         [...nav.querySelectorAll('.user')].map(el => { el.style.display = 'none' });
         [...nav.querySelectorAll('.guest')].map(el => { el.style.display = 'block' });
+        sections[0].querySelector('section#add-movie-button').style.display = 'none';
     } else {
         const email = sessionStorage.getItem('userEmail');
         nav.querySelector('#welcome-msg').textContent = `Welcome, ${email}`;
 
         [...nav.querySelectorAll('.user')].map(el => { el.style.display = 'block' });
         [...nav.querySelectorAll('.guest')].map(el => { el.style.display = 'none' });
+        sections[0].querySelector('section#add-movie-button').style.display = 'block';
     }
 }
 
@@ -54,6 +38,7 @@ const anchorTags = {
     "loginLink": showLogin,
     "registerLink": showRegister,
 };
+navLinks();
 
 function navLinks() {
     nav.addEventListener('click', (e) => {
