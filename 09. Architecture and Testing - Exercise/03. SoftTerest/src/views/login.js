7e266d7cb620d7fa5b38bdef1af9ce5328d54a00
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-14 11:56:03 +0200

    Architecture and Testing

--- 09. Architecture and Testing - Exercise/03. SoftTerest/src/views/login.js
+++ 09. Architecture and Testing - Exercise/03. SoftTerest/src/views/login.js
@@ -1,5 +1,22 @@
-const section = document.querySelector('#loginView');
+import { login } from '../api/user.js';
 
-export function showLogin(context){
+const section = document.querySelector('#loginView');
+const form = section.querySelector('form');
+form.addEventListener('submit', onSubmit);
+let ctx = null;
+
+export function showLogin(context) {
+    ctx = context;
     context.showSection(section);
+}
+
+async function onSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const { email, password } = Object.fromEntries(formData);
+
+    await login( email, password );
+    form.reset();
+    ctx.goTo('/');
 }
