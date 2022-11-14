7483cb1bc2b17d90140fd3de8b8ad262facb1a3f
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-14 15:08:09 +0200

    Architecture and Testing

--- 09. Architecture and Testing - Exercise/03. SoftTerest/src/api/data.js
+++ 09. Architecture and Testing - Exercise/03. SoftTerest/src/api/data.js
@@ -2,8 +2,14 @@ import * as api from "./api.js";
 
 
 const endpoints = {
-    'getIdeas': 'data/ideas?select=_id%2Ctitle%2Cimg&sortBy=_createdOn%20desc'
+    'getIdeas': 'data/ideas?select=_id%2Ctitle%2Cimg&sortBy=_createdOn%20desc',
+    'createIdea': 'data/ideas'
 }
+
 export async function getIdeas(){
-    return api.get(endpoints.getIdeas)
+    return api.get(endpoints.getIdeas);
+}
+
+export async function createIdea(data){
+    return api.post(endpoints.createIdea, data)
 }
