836c17dad638225fa21f1c63ecd8ebc276330343
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-14 15:07:11 +0200

    Architecture and Testing

--- 09. Architecture and Testing - Exercise/03. SoftTerest/src/views/create.js
+++ 09. Architecture and Testing - Exercise/03. SoftTerest/src/views/create.js
@@ -1,5 +1,37 @@
-const section = document.querySelector('#createView');
+import { createIdea } from "../api/data.js";
 
-export function showCreate(context){
+const section = document.querySelector('#createView');
+const form = section.querySelector('form');
+form.addEventListener('submit', onSubmit);
+let ctx;
+
+export function showCreate(context) {
+    ctx = context;
     context.showSection(section);
+}
+
+async function onSubmit(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const { title, description, imageURL } = Object.fromEntries(formData);
+
+    if (title.length < 6) {
+        alert('Title should be at least 6 charecters long!');
+        return;
+    }
+
+    if (description < 10) {
+        alert('Description should be at least 10 charecters long!');
+        return;
+    }
+
+    if (imageURL.length < 5) {
+        alert('Image should be at least 5 charecters long!');
+        return;
+    }
+
+    await createIdea({ title, description, img: imageURL });
+    form.reset();
+    ctx.goTo('/catalog');
 }
