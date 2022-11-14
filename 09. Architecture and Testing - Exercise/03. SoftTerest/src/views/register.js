95845d4b3852ad8d2f26312f979f911b8b0a8749
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-14 11:56:18 +0200

    Architecture and Testing

--- 09. Architecture and Testing - Exercise/03. SoftTerest/src/views/register.js
+++ 09. Architecture and Testing - Exercise/03. SoftTerest/src/views/register.js
@@ -1,5 +1,33 @@
-const section = document.querySelector('#registerView');
+import { register } from "../api/user.js";
 
-export function showRegister(context){
+const section = document.querySelector('#registerView');
+const form = section.querySelector('form');
+form.addEventListener('submit', onRegister);
+let ctx;
+
+export function showRegister(context) {
+    ctx = context;
     context.showSection(section);
+}
+
+async function onRegister(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const { email, password, repeatPassword } = Object.fromEntries(formData);
+
+    if (email.length < 3) {
+        alert('Please enter a valid email address!');
+        return;
+    }
+    if (password.length < 3) {
+        alert('Password should be 3 characters long at least!');
+        return;
+    }
+    if (password !== repeatPassword) {
+        alert('Passwords don\'t match!');
+        return;
+    }
+    await register(email, password);
+    ctx.goTo('/');
 }
