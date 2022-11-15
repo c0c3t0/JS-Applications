ce0f86d5eb45a21303a34dfa8ea95e93596dddb2
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-15 18:14:01 +0200

    Client Side Rendering

---
+++ 11. Client Side Rendering - Exercise/02.HTTP-Status-Cats/app.js
@@ -0,0 +1,42 @@
+import { html, render } from './node_modules/lit-html/lit-html.js';
+import { cats } from './catSeeder.js';
+
+const section = document.querySelector('#allCats');
+
+const template = html`
+    <ul>
+        ${cats.map(cat => createCatCard(cat))}
+    </ul>`;
+
+function createCatCard(cat) {
+    const catTemplate = html`
+    <li>
+        <img src="../images/${cat.imageLocation}.jpg " width="250" height="250" alt="Card image cap">
+        <div class="info">
+            <button class="showBtn" @click=${showDetails}>Show status code</button>
+            <div class="status" style="display: none" id=${cat.id}>
+                <h4>Status Code: ${cat.statusCode}</h4>
+                <p>${cat.statusMessage}</p>
+            </div>
+        </div>
+    </li>`
+
+    return catTemplate;
+}
+
+render(template, section);
+
+function showDetails(e) {
+    e.preventDefault();
+    const button = e.target;
+    const parent = e.target.parentElement;
+    const details = parent.querySelector('.status');
+
+    if (details.style.display === 'block') {
+        details.style.display = 'none';
+        button.textContent = 'Hide status code';
+    } else {
+        details.style.display = 'block';
+        button.textContent = 'Show status code';
+    };
+}
