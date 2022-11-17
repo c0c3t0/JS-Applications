81d8191629790266686f3f4dff9b6b4ee944b806
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-17 02:16:53 +0200

    Client Side Rendering

--- 11. Client Side Rendering - Exercise/06.Book-Library/app.js
+++ 11. Client Side Rendering - Exercise/06.Book-Library/app.js
@@ -1,1 +1,10 @@
-console.log('My requests...')
+import { render } from './node_modules/lit-html/lit-html.js';
+import { tableBodyTemplate, createFormTemplate, editFormTemplate } from './templates.js';
+
+const body = document.querySelector('body');
+
+render([
+    tableBodyTemplate,
+    createFormTemplate,
+    editFormTemplate
+], body);
