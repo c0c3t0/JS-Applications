2a3c0240556cb6e94d4531b24cb6490c2879e18e
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-18 15:40:23 +0200

    Routing

--- 13. Routing - Exercise/01.Furniture/src/api/user.js
+++ 13. Routing - Exercise/01.Furniture/src/api/user.js
@@ -4,9 +4,6 @@ const endpoint = {
     'login': 'users/login',
     'register': 'users/register',
     'logout': 'users/logout',
-    'catalog': 'data/catalog',
-    'getById': 'data/catalog/:id',
-    'getMyItems': 'data/catalog?where=_ownerId%3D%22',
 }
 
 export async function login(email, password) {
@@ -24,35 +21,3 @@ export async function logout() {
     sessionStorage.removeItem('user');
 }
 
-export async function createItem(data){
-    const result = await api.post(endpoint.catalog, data);
-    return result;
-}
-
-export async function getAllItems(){
-    const result = await api.get(endpoint.catalog);
-    return result;
-}
-
-export async function getById(id){
-    const result = await api.get(endpoint.getById + id);
-    return result;
-}
-
-export async function updateById(id, data){
-    const result = await api.put(endpoint.getById + id, data);
-    return result;
-}
-
-export async function deleteById(id){
-    const result = await api.del(endpoint.getById + id);
-    return result;
-}
-
-export async function getMyItems(id){
-    const user = JSON.parse(sessionStorage.getItem('user'));
-    const userId = user && user._id;
-    const id = `${userId}%22`
-    const result = await api.get(endpoint.getMyItems + id);
-    return result;
-}
