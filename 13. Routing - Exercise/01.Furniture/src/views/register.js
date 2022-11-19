68e9da9a87ff6770b983e700c138ed1caf359ea9
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-19 03:02:18 +0200

    Routing

--- 13. Routing - Exercise/01.Furniture/src/views/register.js
+++ 13. Routing - Exercise/01.Furniture/src/views/register.js
@@ -1,14 +1,41 @@
 import { html } from '../lit.js';
 import { register } from '../api/user.js';
 
-let page;
+const registerTemplate = html`
+    <div class="row space-top">
+        <div class="col-md-12">
+            <h1>Register New User</h1>
+            <p>Please fill all fields.</p>
+        </div>
+    </div>
+    <form @submit=${getFormData}>
+        <div class="row space-top">
+            <div class="col-md-4">
+                <div class="form-group">
+                    <label class="form-control-label" for="email">Email</label>
+                    <input class="form-control" id="email" type="text" name="email">
+                </div>
+                <div class="form-group">
+                    <label class="form-control-label" for="password">Password</label>
+                    <input class="form-control" id="password" type="password" name="password">
+                </div>
+                <div class="form-group">
+                    <label class="form-control-label" for="rePass">Repeat</label>
+                    <input class="form-control" id="rePass" type="password" name="rePass">
+                </div>
+                <input type="submit" class="btn btn-primary" value="Register" />
+            </div>
+        </div>
+    </form>`;
 
-export function showRegister(ctx) {
-    page = ctx.page;
-    ctx.render(registerTemplate());
+let ctx = null;
+
+export function showRegister(context) {
+    ctx = context;
+    ctx.render(registerTemplate);
 }
 
-function getFormData(e) {
+async function getFormData(e) {
     e.preventDefault();
     const formData = new FormData(e.target);
     const { email, password, rePass } = Object.fromEntries(formData);
@@ -16,41 +43,13 @@ function getFormData(e) {
         alert('All fields are required!');
         return;
     }
-    if(password !== rePass){
+    if (password !== rePass) {
         alert('Passwords don\'t match!');
         return;
     }
 
-    register(email, password);
+    await register(email, password);
     e.target.reset();
-    page.redirect('/catalog');
-}
-
-function registerTemplate() {
-    return html`
-    <div class="row space-top">
-            <div class="col-md-12">
-                <h1>Register New User</h1>
-                <p>Please fill all fields.</p>
-            </div>
-        </div>
-        <form @submit=${getFormData}>
-            <div class="row space-top">
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label class="form-control-label" for="email">Email</label>
-                        <input class="form-control" id="email" type="text" name="email">
-                    </div>
-                    <div class="form-group">
-                        <label class="form-control-label" for="password">Password</label>
-                        <input class="form-control" id="password" type="password" name="password">
-                    </div>
-                    <div class="form-group">
-                        <label class="form-control-label" for="rePass">Repeat</label>
-                        <input class="form-control" id="rePass" type="password" name="rePass">
-                    </div>
-                    <input type="submit" class="btn btn-primary" value="Register" />
-                </div>
-            </div>
-        </form>`;
+    ctx.updateNavigation();
+    ctx.page.redirect('/catalog');
 }
