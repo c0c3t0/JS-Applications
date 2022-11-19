ef6ed936474920f653ba7aa097bec8b7e3bd173b
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-19 03:01:24 +0200

    Routing

--- 13. Routing - Exercise/01.Furniture/src/lit.js
+++ 13. Routing - Exercise/01.Furniture/src/lit.js
@@ -1,2 +1,2 @@
 export { default as page } from '../node_modules/page/page.mjs';
-export { html, render } from '../node_modules/lit-html/lit-html.js';
+export { html, render, nothing } from '../node_modules/lit-html/lit-html.js';
