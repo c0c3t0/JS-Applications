254e3dd932329bd9791e5f6d28f696843736d3b6
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-20 20:33:46 +0200

    Solution

---
+++ 16. Exam Preparation/01. JS Applications Retake Exam - 15 August 2022/Sole Mates_Resources/auth/login.js
@@ -0,0 +1,47 @@
+import { html } from '../lit.js';
+import { login } from '../api/user.js';
+
+//===============> CHANGES NEED TO BE MADE <===============
+// [x] loginTemplate;
+
+const loginTemplate = html`
+    <section id="login">
+          <div class="form">
+            <h2>Login</h2>
+            <form @submit=${getFormData} class="login-form">
+              <input type="text" name="email" id="email" placeholder="email" />
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="password"
+              />
+              <button type="submit">login</button>
+              <p class="message">
+                Not registered? <a href="#">Create an account</a>
+              </p>
+            </form>
+          </div>
+        </section>`;
+
+let ctx = null;
+
+export function showLogin(context) {
+    ctx = context;
+    ctx.render(loginTemplate);
+}
+
+async function getFormData(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const { email, password } = Object.fromEntries(formData);
+    if (!email || !password) {
+        alert('All fields are required!');
+        return;
+    }
+
+    await login(email, password);
+    e.target.reset();
+    ctx.updateNavigation();
+    ctx.page.redirect('/dashboard');
+}
