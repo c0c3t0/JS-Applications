3c97df28cd4bb59ffaba7b1eddd5f013e79e1638
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-20 20:35:06 +0200

    Solution

---
+++ 16. Exam Preparation/01. JS Applications Retake Exam - 15 August 2022/Sole Mates_Resources/app.js
@@ -0,0 +1,57 @@
+import { logout } from './api/user.js';
+import { page, render } from './lit.js';
+// import { showDashboard } from './views/dashboard.js';
+// import { showCreate } from './views/create.js';
+// import { showDetails } from './views/details.js';
+// import { showEdit } from './views/edit.js';
+import { showLogin } from './auth/login.js';
+// import { showMyFurniture } from './views/ownerCatalog.js';
+import { showRegister } from './auth/register.js';
+
+//===============> CHANGES NEED TO BE MADE <===============
+// [x] paths in page;
+// [x] navigation;
+
+const nav = document.querySelector('nav');
+const root = document.querySelector('main');
+// nav.getElementById('logoutBtn').addEventListener('click', onLogout);
+
+page(decorateContext);
+// page('/index', '/');
+// page('/', showDashboard);
+
+page('/login', showLogin);
+page('/register', showRegister);
+page('/logout', onLogout);
+
+// page('/catalog', showCatalog);
+// page('/create', showCreate);
+// // page('/details/:id', showDetails);
+// // page('/edit/:id', showEdit);
+// // page('/my-furniture', showMyFurniture);
+
+page.start();
+updateNavigation();
+
+function decorateContext(ctx, next) {
+    ctx.updateNavigation = updateNavigation;
+    ctx.render = (content) => render(content, root);
+    next();
+}
+ 
+function updateNavigation() {
+    const user = sessionStorage.getItem('user');
+    if (user) {
+        nav.querySelector('.user').style.display = 'inline-block';
+        nav.querySelector('.guest').style.display = 'none';
+    } else {
+        nav.querySelector('.user').style.display = 'none';
+        nav.querySelector('.guest').style.display = 'inline-block';
+    }
+}
+
+async function onLogout() {
+    await logout();
+    updateNavigation(); 
+    page.redirect('/dashboard');
+}
