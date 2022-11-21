6cb54487b36c50c648f973ed0dced578d2467680
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-21 15:53:07 +0200

    Solution

---
+++ 16. Exam Preparation/02. JS Applications Exam - 06 August 2022/Clear Career_Resources/src/views/home.js
@@ -0,0 +1,12 @@
+import { html } from "../lit.js";
+
+const homeTemplate = html`
+<section id="home">
+    <img src="./images/pngkey.com-hunting-png-6697165-removebg-preview.png" alt="home" />
+    <h2>Searching for a job?</h2>
+    <h3>The right place for a new career start!</h3>
+</section>`;
+
+export function showHome(context) {
+    context.render(homeTemplate);
+}
