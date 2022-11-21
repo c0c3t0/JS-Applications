1b19b066c3679f2698390c7f6c24f0c747ed3e79
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-21 15:52:26 +0200

    Solution

---
+++ 16. Exam Preparation/02. JS Applications Exam - 06 August 2022/Clear Career_Resources/src/auth/register.js
@@ -0,0 +1,43 @@
+import { html } from '../lit.js';
+import { register } from '../api/user.js';
+
+const registerTemplate = html`
+    <section id="register">
+        <div class="form">
+            <h2>Register</h2>
+            <form @submit=${getFormData} class="login-form">
+                <input type="text" name="email" id="register-email" placeholder="email" />
+                <input type="password" name="password" id="register-password" placeholder="password" />
+                <input type="password" name="re-password" id="repeat-password" placeholder="repeat password" />
+                <button type="submit">register</button>
+                <p class="message">Already registered? <a href="/login">Login</a></p>
+            </form>
+        </div>
+    </section>`;
+
+let ctx = null;
+
+export function showRegister(context) {
+    ctx = context;
+    ctx.render(registerTemplate);
+}
+
+async function getFormData(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+    console.log(data)
+    if (!data.email || !data.password || !data['re-password']) {
+        alert('All fields are required!');
+        return;
+    }
+    if (data.password !== data['re-password']) {
+        alert('Passwords don\'t match!');
+        return;
+    }
+
+    await register(data.email, data.password);
+    e.target.reset();
+    ctx.updateNavigation();
+    ctx.page.redirect('/dashboard');
+}
