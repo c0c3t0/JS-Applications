ae7bafff8ce3be4357dc1566a865c341b53671bb
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-21 04:49:55 +0200

    Solution

--- 16. Exam Preparation/01. JS Applications Retake Exam - 15 August 2022/Sole Mates_Resources/api/data.js
+++ 16. Exam Preparation/01. JS Applications Retake Exam - 15 August 2022/Sole Mates_Resources/api/data.js
@@ -4,6 +4,7 @@ const endpoint = {
     'getItems': 'data/shoes?sortBy=_createdOn%20desc',
     'create': 'data/shoes',
     'getById': 'data/shoes/',
+    'search': (query) => `data/shoes?where=brand%20LIKE%20%22${query}%22`
 }
 
 export async function createItem(data){
@@ -30,3 +31,7 @@ export async function deleteById(id){
     const result = await api.del(endpoint.getById + id);
     return result;
 }
+
+export async function searchItem(query) {
+    return await api.get(endpoint.search(query));
+  }
