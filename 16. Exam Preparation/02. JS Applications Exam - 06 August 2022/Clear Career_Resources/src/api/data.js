d467ca57a2bbe1de6c31ad6318bda4a0305409b7
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-21 15:51:48 +0200

    Solution

---
+++ 16. Exam Preparation/02. JS Applications Exam - 06 August 2022/Clear Career_Resources/src/api/data.js
@@ -0,0 +1,41 @@
+import * as api from "./api.js";
+
+const endpoint = {
+    'catalog': 'data/offers?sortBy=_createdOn%20desc',
+    'create': 'data/offers',
+    'getById': 'data/catalog/',
+    // 'getMyItems': 'data/catalog?where=_ownerId%3D%22',
+}
+
+export async function createItem(data){
+    const result = await api.post(endpoint.create, data);
+    return result;
+}
+
+export async function getAllItems(){
+    const result = await api.get(endpoint.catalog);
+    return result;
+}
+
+export async function getById(id){
+    const result = await api.get(endpoint.getById + id);
+    return result;
+}
+
+export async function updateById(id, data){
+    const result = await api.put(endpoint.getById + id, data);
+    return result;
+}
+
+export async function deleteById(id){
+    const result = await api.del(endpoint.getById + id);
+    return result;
+}
+
+// export async function getMyItems(){
+//     const user = JSON.parse(sessionStorage.getItem('user'));
+//     const userId = user && user._id;
+//     const id = `${userId}%22`;
+//     const result = await api.get(endpoint.getMyItems + id);
+//     return result;
+// }
