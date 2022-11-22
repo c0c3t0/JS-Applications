6a989aa57abd730278c89dda09c73c2a4aeefc5e
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-22 02:11:46 +0200

    Solution

--- 16. Exam Preparation/02. JS Applications Exam - 06 August 2022/Clear Career_Resources/src/api/data.js
+++ 16. Exam Preparation/02. JS Applications Exam - 06 August 2022/Clear Career_Resources/src/api/data.js
@@ -3,8 +3,10 @@ import * as api from "./api.js";
 const endpoint = {
     'catalog': 'data/offers?sortBy=_createdOn%20desc',
     'create': 'data/offers',
-    'getById': 'data/catalog/',
-    // 'getMyItems': 'data/catalog?where=_ownerId%3D%22',
+    'getById': 'data/offers/',
+    'application': 'data/applications',
+    'totalApplications': (offerId) => `data/applications?where=offerId%3D%22${offerId}%22&distinct=_ownerId&count`,
+    'applicationsByUser': (offerId, userId) => `data/applications?where=offerId%3D%22${offerId}%22%20and%20_ownerId%3D%22${userId}%22&count`
 }
 
 export async function createItem(data){
@@ -32,10 +34,17 @@ export async function deleteById(id){
     return result;
 }
 
-// export async function getMyItems(){
-//     const user = JSON.parse(sessionStorage.getItem('user'));
-//     const userId = user && user._id;
-//     const id = `${userId}%22`;
-//     const result = await api.get(endpoint.getMyItems + id);
-//     return result;
-// }
+export async function apply(data){
+    const result = await api.post(endpoint.application, data);
+    return result;
+}
+
+export async function getTotalCount(offerId){
+    const result = await api.get(endpoint.totalApplications(offerId));
+    return result;
+}
+
+export async function applied(offerId, userId){
+    const result = await api.get(endpoint.applicationsByUser(offerId, userId));
+    return result;
+}
