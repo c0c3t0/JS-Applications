b737204697b5aab023bde687154ed5e6a64aa366
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-22 02:12:12 +0200

    Solution

---
+++ 16. Exam Preparation/02. JS Applications Exam - 06 August 2022/Clear Career_Resources/src/views/details.js
@@ -0,0 +1,77 @@
+import { applied, apply, deleteById, getById, getTotalCount } from '../api/data.js';
+import { html, nothing } from '../lit.js';
+
+const detailsTemplate = (data, isOwner, isLogged, hasApplied, onApply, totalCount) => html`
+<section id="details">
+    <div id="details-wrapper">
+        <img id="details-img" src=${data.imageUrl} alt="example1" />
+        <p id="details-title">${data.title}</p>
+        <p id="details-category">
+            Category: <span id="categories">${data.category}</span>
+        </p>
+        <p id="details-salary">
+            Salary: <span id="salary-number">${data.salary}</span>
+        </p>
+        <div id="info-wrapper">
+            <div id="details-description">
+                <h4>Description</h4>
+                <span>${data.description}</span>
+            </div>
+            <div id="details-requirements">
+                <h4>Requirements</h4>
+                <span>${data.requirements}</span>
+            </div>
+        </div>
+        <p>Applications: <strong id="applications">${totalCount}</strong></p>
+
+        <div id="action-buttons">
+            ${isOwner
+        ? html`
+            <a href="/edit/${data._id}" id="edit-btn">Edit</a>
+            <a @click=${deleteItem} href="#" id="delete-btn">Delete</a>`
+        : nothing}
+            ${isLogged && !isOwner && hasApplied == 0 ? html`<a @click=${onApply} href="#" id="apply-btn">Apply</a>` :
+            nothing}
+        </div>
+    </div>
+    </div>
+`;
+
+let ctx = null;
+
+export async function showDetails(context) {
+    ctx = context;
+    const offerId = ctx.params.id;
+    const data = await getById(offerId);
+    const totalCount = await getTotalCount(offerId);
+
+    const user = JSON.parse(sessionStorage.getItem('user'));
+    const isOwner = user ? user._id === data._ownerId : false;
+    const isLogged = !user ? false : true;
+    let hasApplied = 0;
+
+    if (isLogged && !isOwner) {
+        hasApplied = await applied(offerId, user._id);
+    }
+
+    ctx.render(detailsTemplate(data, isOwner, isLogged, hasApplied, onApply, totalCount));
+
+    async function onApply(e) {
+        e.preventDefault();
+        await apply({ offerId });
+        const totalCount = await getTotalCount(offerId);
+        hasApplied = await applied(offerId, user._id);
+
+        ctx.render(detailsTemplate(data, isOwner, isLogged, hasApplied, onApply, totalCount));
+    }
+}
+
+async function deleteItem(e) {
+    e.preventDefault();
+    const confirmed = confirm('Are you sure you want to delete this item?');
+    if (confirmed) {
+        await deleteById(ctx.params.id);
+        ctx.page.redirect('/dashboard');
+    }
+}
+
