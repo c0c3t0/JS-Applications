f9ab0a4a869c7f16f1cb9e452b9d91139119e9fd
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-22 16:32:52 +0200

    Solution

---
+++ 16. Exam Preparation/03. JS Applications Retake Exam - 10 April 2022/Local Orphanages_Ресурси/LocalOrphanages_Resources/src/views/dashboard.js
@@ -0,0 +1,28 @@
+import { html } from '../lit.js';
+import { getAllItems } from "../api/data.js";
+
+const dashboardTemplate = (data) => html`
+<section id="dashboard-page">
+    <h1 class="title">All Posts</h1>
+    ${data.length === 0 ? html`<h1 class="title no-posts-title">No posts yet!</h1>` : data.map(itemCard)};
+</section>`;
+
+const itemCard = (item) => html`
+<div class="all-posts">
+    <div class="post">
+        <h2 class="post-title">${item.title}</h2>
+        <img class="post-image" src=${item.imageUrl} alt="Material Image">
+        <div class="btn-wrapper">
+            <a href="/details/${item._id}" class="details-btn btn">Details</a>
+        </div>
+    </div>
+</div>`;
+
+let ctx;
+
+export async function showDashboard(context) {
+    ctx = context;
+    const data = await getAllItems();
+    ctx.render(dashboardTemplate(data));
+    console.log(data);
+}
