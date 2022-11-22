fd256f10399abd6da5aa55beca5472f2cc5e135c
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-22 16:32:26 +0200

    Solution

---
+++ 16. Exam Preparation/03. JS Applications Retake Exam - 10 April 2022/Local Orphanages_Ресурси/LocalOrphanages_Resources/src/auth/register.js
@@ -0,0 +1,53 @@
+import { html } from '../lit.js';
+import { register } from '../api/user.js';
+
+const registerTemplate = html`
+    <section id="register-page" class="auth">
+        <form @submit=${getFormData} id="register">
+            <h1 class="title">Register</h1>
+    
+            <article class="input-group">
+                <label for="register-email">Email: </label>
+                <input type="email" id="register-email" name="email">
+            </article>
+    
+            <article class="input-group">
+                <label for="register-password">Password: </label>
+                <input type="password" id="register-password" name="password">
+            </article>
+    
+            <article class="input-group">
+                <label for="repeat-password">Repeat Password: </label>
+                <input type="password" id="repeat-password" name="repeatPassword">
+            </article>
+    
+            <input type="submit" class="btn submit-btn" value="Register">
+        </form>
+    </section>`;
+
+let ctx = null;
+
+export function showRegister(context) {
+    ctx = context;
+    ctx.render(registerTemplate);
+}
+
+async function getFormData(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const { email, password, repeatPassword } = Object.fromEntries(formData);
+
+    if (!email || !password || !repeatPassword) {
+        alert('All fields are required!');
+        return;
+    }
+    if (password !== repeatPassword) {
+        alert('Passwords don\'t match!');
+        return;
+    }
+
+    await register(email, password, repeatPassword);
+    e.target.reset();
+    ctx.updateNavigation();
+    ctx.page.redirect('/');
+}
