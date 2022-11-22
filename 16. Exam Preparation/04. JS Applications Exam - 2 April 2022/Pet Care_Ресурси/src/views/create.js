0ef3ced8ac532b1a2d204eb9d453849f6302db38
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-23 01:10:41 +0200

    Solution

---
+++ 16. Exam Preparation/04. JS Applications Exam - 2 April 2022/Pet Care_Ресурси/src/views/create.js
@@ -0,0 +1,56 @@
+import { createItem } from '../api/data.js';
+import { html } from '../lit.js';
+
+const createTemplate = html`
+<section id="createPage">
+    <form @submit=${getFormData} class="createForm">
+        <img src="./images/cat-create.jpg">
+        <div>
+            <h2>Create PetPal</h2>
+            <div class="name">
+                <label for="name">Name:</label>
+                <input name="name" id="name" type="text" placeholder="Max">
+            </div>
+            <div class="breed">
+                <label for="breed">Breed:</label>
+                <input name="breed" id="breed" type="text" placeholder="Shiba Inu">
+            </div>
+            <div class="Age">
+                <label for="age">Age:</label>
+                <input name="age" id="age" type="text" placeholder="2 years">
+            </div>
+            <div class="weight">
+                <label for="weight">Weight:</label>
+                <input name="weight" id="weight" type="text" placeholder="5kg">
+            </div>
+            <div class="image">
+                <label for="image">Image:</label>
+                <input name="image" id="image" type="text" placeholder="./image/dog.jpeg">
+            </div>
+            <button class="btn" type="submit">Create Pet</button>
+        </div>
+    </form>
+</section>`;
+
+let ctx = null;
+
+export async function showCreate(context) {
+    ctx = context;
+    ctx.render(createTemplate);
+}
+
+async function getFormData(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+    const { name, breed, age, weight, image } = data;
+
+    if (!name || !breed || !age || !weight || !image) {
+        alert('All fields are required!');
+        return;
+    }
+
+    await createItem(data);
+    e.target.reset();
+    ctx.page.redirect('/');
+}
