f17b1af5d3d8604c204a931f86544593256b22bb
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-22 16:33:03 +0200

    Solution

---
+++ 16. Exam Preparation/03. JS Applications Retake Exam - 10 April 2022/Local Orphanages_Ресурси/LocalOrphanages_Resources/src/views/details.js
@@ -0,0 +1,72 @@
+import { deleteById, donate, donateByUser, getById, getTotalCount } from '../api/data.js';
+import { html, nothing } from '../lit.js';
+
+const detailsTemplate = (data, isOwner, isLogged, hasDonate, onDonate, totalCount) => html`
+<section id="details-page">
+    <h1 class="title">Post Details</h1>
+
+    <div id="container">
+        <div id="details">
+            <div class="image-wrapper">
+                <img src=${data.imageUrl} alt="Material Image" class="post-image">
+            </div>
+            <div class="info">
+                <h2 class="title post-title">${data.title}</h2>
+                <p class="post-description">Description: ${data.description} </p>
+                <p class="post-address">Address: ${data.address}</p>
+                <p class="post-number">Phone number: ${data.phone}</p>
+                <p class="donate-Item">Donate Materials: ${totalCount}</p>
+                <div class="btns">
+                    ${isOwner
+                        ? html`
+                            <a href="/edit/${data._id}" class="edit-btn btn">Edit</a>
+                            <a @click=${deleteItem} href="#" class="delete-btn btn">Delete</a>`
+                        : nothing}
+                    ${isLogged && !isOwner && hasDonate == 0 
+                        ? html`<a @click=${onDonate} href="#" class="donate-btn btn">Donate</a>` 
+                        : nothing}
+                </div>
+            </div>
+        </div>
+    </div>
+</section>`;
+
+let ctx = null;
+
+export async function showDetails(context) {
+    ctx = context;
+    const postId = ctx.params.id;
+    const data = await getById(postId);
+    const totalCount = await getTotalCount(postId);
+
+    const user = JSON.parse(sessionStorage.getItem('user'));
+    const isOwner = user ? user._id === data._ownerId : false;
+    const isLogged = !user ? false : true;
+    let hasDonate = 0;
+
+    if (isLogged && !isOwner) {
+        hasDonate = await donateByUser(postId, user._id);
+    }
+
+    ctx.render(detailsTemplate(data, isOwner, isLogged, hasDonate, onDonate, totalCount));
+
+    async function onDonate(e) {
+        e.preventDefault();
+        await donate({ postId });
+        const totalCount = await getTotalCount(postId);
+
+        hasDonate = await donateByUser(postId, user._id);
+
+        ctx.render(detailsTemplate(data, isOwner, isLogged, hasDonate, onDonate, totalCount));
+    }
+}
+
+async function deleteItem(e) {
+    e.preventDefault();
+    const confirmed = confirm('Are you sure you want to delete this item?');
+    if (confirmed) {
+        await deleteById(ctx.params.id);
+        ctx.page.redirect('/');
+    }
+}
+
