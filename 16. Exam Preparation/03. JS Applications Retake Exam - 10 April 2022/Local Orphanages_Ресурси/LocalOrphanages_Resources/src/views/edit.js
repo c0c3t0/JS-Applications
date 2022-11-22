2b9f96f2d1e5e33d8d5c68329f2529f129bfea95
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-22 16:33:30 +0200

    Solution

---
+++ 16. Exam Preparation/03. JS Applications Retake Exam - 10 April 2022/Local Orphanages_Ресурси/LocalOrphanages_Resources/src/views/edit.js
@@ -0,0 +1,62 @@
+import { getById, updateById } from '../api/data.js';
+import { html } from '../lit.js';
+
+const editTemplate = (item) => html`
+<section id="edit-page" class="auth">
+    <form @submit=${getFormData} id="edit">
+        <h1 class="title">Edit Post</h1>
+
+        <article class="input-group">
+            <label for="title">Post Title</label>
+            <input type="title" name="title" id="title" value=${item.title}>
+        </article>
+
+        <article class="input-group">
+            <label for="description">Description of the needs </label>
+            <input type="text" name="description" id="description" value=${item.description}>
+        </article>
+
+        <article class="input-group">
+            <label for="imageUrl"> Needed materials image </label>
+            <input type="text" name="imageUrl" id="imageUrl" value=${item.imageUrl}>
+        </article>
+
+        <article class="input-group">
+            <label for="address">Address of the orphanage</label>
+            <input type="text" name="address" id="address" value=${item.address}>
+        </article>
+
+        <article class="input-group">
+            <label for="phone">Phone number of orphanage employee</label>
+            <input type="text" name="phone" id="phone" value=${item.phone}>
+        </article>
+
+        <input type="submit" class="btn submit" value="Edit Post">
+    </form>
+</section>`;
+
+let ctx = null;
+
+export async function showEdit(context) {
+    ctx = context;
+    const data = await getById(ctx.params.id);
+    console.log(data);
+    context.render(editTemplate(data));
+}
+
+async function getFormData(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+    const { title, description, imageUrl, address, phone } = data;
+
+
+    if (!title || !description || !imageUrl || !address || !phone) {
+        alert('All fields are required!');
+        return;
+    }
+
+    await updateById(ctx.params.id, data);
+    e.target.reset();
+    ctx.page.redirect(`/details/${ctx.params.id}`);
+}
