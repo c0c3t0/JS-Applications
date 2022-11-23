5e3887b707f50f7599d2a097e1a2edb660b9ff58
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-23 15:58:31 +0200

    Solution

---
+++ 16. Exam Preparation/05. JS Applications Exam Preparation - Meme Lounge/Meme Lounge_Ресурси/Meme_Lounge/src/views/home.js
@@ -0,0 +1,18 @@
+import { html } from "../lit.js";
+
+const homeTemplate = html`
+<section id="welcome">
+    <div id="welcome-container">
+        <h1>Welcome To Meme Lounge</h1>
+        <img src="/images/welcome-meme.jpg" alt="meme">
+        <h2>Login to see our memes right away!</h2>
+        <div id="button-div">
+            <a href="/login" class="button">Login</a>
+            <a href="/register" class="button">Register</a>
+        </div>
+    </div>
+</section>`;
+
+export function showHome(context) {
+    context.render(homeTemplate);
+}
