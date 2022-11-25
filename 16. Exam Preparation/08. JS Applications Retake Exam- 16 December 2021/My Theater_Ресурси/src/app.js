9a5b6142ca08d6ebef486c7405d21c5db20e55ca
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-26 01:25:52 +0200

    Solution

---
+++ 16. Exam Preparation/08. JS Applications Retake Exam- 16 December 2021/My Theater_Ресурси/src/app.js
@@ -0,0 +1,52 @@
+import { logout } from './api/user.js';
+import { page, render } from './lit.js';
+import { showCreate } from './views/create.js';
+import { showDetails } from './views/details.js';
+import { showEdit } from './views/edit.js';
+import { showLogin } from './auth/login.js';
+import { showRegister } from './auth/register.js';
+import { showHome } from './views/home.js';
+import { showProfile } from './views/profile.js';
+
+const root = document.querySelector('#content');
+const nav = document.querySelector('nav');
+
+page(decorateContext)
+page('/index.html', showHome);
+page('/', showHome);
+
+page('/login', showLogin);
+page('/register', showRegister);
+page('/logout', onLogout);
+
+page('/create', showCreate);
+page('/details/:id', showDetails);
+page('/edit/:id', showEdit);
+page('/profile', showProfile);
+
+
+page.start();
+updateNavigation();
+
+function decorateContext(ctx, next) {
+    ctx.updateNavigation = updateNavigation;
+    ctx.render = (content) => render(content, root);
+    next();
+}
+ 
+function updateNavigation() {
+    const user = sessionStorage.getItem('user');
+    if (user) {
+        [...nav.querySelectorAll('.user')].map(el => el.style.display = 'inline-block');
+        [...nav.querySelectorAll('.guest')].map(el => el.style.display = 'none');
+    } else {
+        [...nav.querySelectorAll('.user')].map(el => el.style.display = 'none');
+        [...nav.querySelectorAll('.guest')].map(el => el.style.display = 'inline-block');
+    }
+}
+
+async function onLogout() {
+    await logout();
+    updateNavigation();
+    page.redirect('/');
+}
