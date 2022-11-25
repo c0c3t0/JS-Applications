83483b54dd1a7fd0a4b10a4e04522fe1010bed3b
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-26 01:25:19 +0200

    Solution

---
+++ 16. Exam Preparation/08. JS Applications Retake Exam- 16 December 2021/My Theater_Ресурси/src/views/home.js
@@ -0,0 +1,49 @@
+import { html } from '../lit.js';
+import { getAllItems } from "../api/data.js";
+
+const homeTemplate = (data) => html`
+<section class="welcomePage">
+    <div id="welcomeMessage">
+        <h1>My Theater</h1>
+        <p>Since 1962 World Theatre Day has been celebrated by ITI Centres, ITI Cooperating Members, theatre
+            professionals, theatre organizations, theatre universities and theatre lovers all over the world on
+            the 27th of March. This day is a celebration for those who can see the value and importance of the
+            art
+            form “theatre”, and acts as a wake-up-call for governments, politicians and institutions which have
+            not
+            yet recognised its value to the people and to the individual and have not yet realised its potential
+            for
+            economic growth.</p>
+    </div>
+    <div id="events">
+        <h1>Future Events</h1>
+        <div class="theaters-container">
+            ${data.length > 0 ? data.map(item => itemCardTemplate(item)) : html`<h4 class="no-event">No Events Yet...
+            </h4>`}
+        </div>
+    </div>
+</section>`;
+
+const itemCardTemplate = (item) => html`
+<div class="eventsInfo">
+    <div class="home-image">
+        <img src=${item.imageUrl}>
+    </div>
+    <div class="info">
+        <h4 class="title">${item.title}</h4>
+        <h6 class="date">${item.date}</h6>
+        <h6 class="author">${item.author}</h6>
+        <div class="info-buttons">
+            <a class="btn-details" href="/details/${item._id}">Details</a>
+        </div>
+    </div>
+</div>`;
+
+let ctx;
+
+export async function showHome(context) {
+    ctx = context;
+    const data = await getAllItems();
+    console.log(data);
+    ctx.render(homeTemplate(data));
+}
