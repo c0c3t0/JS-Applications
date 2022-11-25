5b9cd104031fc06fbf9b374b33a78b0e5460a232
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-26 01:26:23 +0200

    Solution

---
+++ 16. Exam Preparation/08. JS Applications Retake Exam- 16 December 2021/My Theater_Ресурси/src/auth/login.js
@@ -0,0 +1,45 @@
+import { html } from '../lit.js';
+import { login } from '../api/user.js';
+
+const loginTemplate = html`
+<section id="loginaPage">
+    <form @submit=${getFormData} class="loginForm">
+        <h2>Login</h2>
+        <div>
+            <label for="email">Email:</label>
+            <input id="email" name="email" type="text" placeholder="steven@abv.bg" value="">
+        </div>
+        <div>
+            <label for="password">Password:</label>
+            <input id="password" name="password" type="password" placeholder="********" value="">
+        </div>
+
+        <button class="btn" type="submit">Login</button>
+
+        <p class="field">
+            <span>If you don't have profile click <a href="/register">here</a></span>
+        </p>
+    </form>
+</section>`;
+
+let ctx = null;
+
+export function showLogin(context) {
+    ctx = context;
+    ctx.render(loginTemplate);
+}
+
+async function getFormData(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const { email, password } = Object.fromEntries(formData);
+    if (!email || !password) {
+        alert('All fields are required!');
+        return;
+    }
+
+    await login(email, password);
+    e.target.reset();
+    ctx.updateNavigation();
+    ctx.page.redirect('/');
+}
