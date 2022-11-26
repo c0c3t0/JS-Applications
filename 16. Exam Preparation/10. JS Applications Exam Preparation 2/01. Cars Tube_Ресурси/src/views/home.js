c84239fc91b99d7726923170cd1a8396510e0bc5
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-27 01:36:15 +0200

    Solution

---
+++ 16. Exam Preparation/10. JS Applications Exam Preparation 2/01. Cars Tube_Ресурси/src/views/home.js
@@ -0,0 +1,17 @@
+import { html } from "../lit.js";
+
+const homeTemplate = html`
+<section id="main">
+    <div id="welcome-container">
+        <h1>Welcome To Car Tube</h1>
+        <img class="hero" src="/images/car-png.webp" alt="carIntro">
+        <h2>To see all the listings click the link below:</h2>
+        <div>
+            <a href="/dashboard" class="button">Listings</a>
+        </div>
+    </div>
+</section>`;
+
+export function showHome(context) {
+    context.render(homeTemplate);
+}
