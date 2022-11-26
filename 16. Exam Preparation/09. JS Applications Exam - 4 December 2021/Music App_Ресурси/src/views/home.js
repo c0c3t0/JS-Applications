1bdab85caeffecb3212a724ed1d1895486309bf8
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-26 17:02:15 +0200

    Solution

---
+++ 16. Exam Preparation/09. JS Applications Exam - 4 December 2021/Music App_Ресурси/src/views/home.js
@@ -0,0 +1,17 @@
+import { html } from "../lit.js";
+
+const homeTemplate = html`
+<section id="welcomePage">
+    <div id="welcome-message">
+        <h1>Welcome to</h1>
+        <h1>My Music Application!</h1>
+    </div>
+
+    <div class="music-img">
+        <img src="./images/musicIcons.webp">
+    </div>
+</section>`;
+
+export function showHome(context) {
+    context.render(homeTemplate);
+}
