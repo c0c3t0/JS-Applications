3d34ff08b85a425aae818c4522a098850046854f
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-28 15:47:12 +0200

    Solution

---
+++ 16. Exam Preparation/06. JS Applications Retake Exam - 16 Aug 2021/GamesPlay_Ресурси/src/app.js
@@ -0,0 +1,50 @@
+import { logout } from './api/user.js';
+import { page, render } from './lit.js';
+import { showDashboard } from './views/dashboard.js';
+import { showCreate } from './views/create.js';
+import { showDetails } from './views/details.js';
+import { showEdit } from './views/edit.js';
+import { showLogin } from './auth/login.js';
+import { showRegister } from './auth/register.js';
+import { showHome } from './views/home.js';
+
+const root = document.querySelector('#main-content');
+
+page(decorateContext)
+page('/index.html', showHome);
+page('/', showHome);
+
+page('/login', showLogin);
+page('/register', showRegister);
+page('/logout', onLogout);
+
+page('/dashboard', showDashboard);
+page('/create', showCreate);
+page('/details/:id', showDetails);
+page('/edit/:id', showEdit);
+
+page.start();
+updateNavigation();
+
+function decorateContext(ctx, next) {
+    ctx.updateNavigation = updateNavigation;
+    ctx.render = (content) => render(content, root);
+    next();
+}
+ 
+function updateNavigation() {
+    const user = JSON.parse(sessionStorage.getItem('user'));
+    if (user) {
+        document.querySelector('#user').style.display = 'inline-block';
+        document.querySelector('#guest').style.display = 'none';
+    } else {
+        document.querySelector('#user').style.display = 'none';
+        document.querySelector('#guest').style.display = 'inline-block';
+    }
+}
+
+async function onLogout() {
+    await logout();
+    updateNavigation();
+    page.redirect('/');
+}
