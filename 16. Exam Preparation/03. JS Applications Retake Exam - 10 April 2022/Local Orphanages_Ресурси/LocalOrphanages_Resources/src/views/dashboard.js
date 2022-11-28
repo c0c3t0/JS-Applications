deb702ade573d6a4db63c27661af92a78da16b83
c0c3t0 <98971277+c0c3t0@users.noreply.github.com>
2022-11-28 02:39:15 +0200

    Solution

--- 16. Exam Preparation/03. JS Applications Retake Exam - 10 April 2022/Local Orphanages_Ресурси/LocalOrphanages_Resources/src/views/dashboard.js
+++ 16. Exam Preparation/03. JS Applications Retake Exam - 10 April 2022/Local Orphanages_Ресурси/LocalOrphanages_Resources/src/views/dashboard.js
@@ -4,17 +4,17 @@ import { getAllItems } from "../api/data.js";
 const dashboardTemplate = (data) => html`
 <section id="dashboard-page">
     <h1 class="title">All Posts</h1>
-    ${data.length === 0 ? html`<h1 class="title no-posts-title">No posts yet!</h1>` : data.map(itemCard)};
+    ${data.length > 0 
+        ? html`<div class="all-posts">${data.map(itemCard)}</div>` 
+        : html`<h1 class="title no-posts-title">No posts yet!</h1>`}
 </section>`;
 
 const itemCard = (item) => html`
-<div class="all-posts">
-    <div class="post">
-        <h2 class="post-title">${item.title}</h2>
-        <img class="post-image" src=${item.imageUrl} alt="Material Image">
-        <div class="btn-wrapper">
-            <a href="/details/${item._id}" class="details-btn btn">Details</a>
-        </div>
+<div class="post">
+    <h2 class="post-title">${item.title}</h2>
+    <img class="post-image" src=${item.imageUrl} alt="Material Image">
+    <div class="btn-wrapper">
+        <a href="/details/${item._id}" class="details-btn btn">Details</a>
     </div>
 </div>`;
 
@@ -24,5 +24,4 @@ export async function showDashboard(context) {
     ctx = context;
     const data = await getAllItems();
     ctx.render(dashboardTemplate(data));
-    console.log(data);
 }
